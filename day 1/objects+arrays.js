1e2d03548ccedb2172ef8a3b77114104a4193652
Jashwanth3 <153377474+Jashwanth3@users.noreply.github.com>
2023-12-28 06:25:48 +0530

    Add files via upload
    basics javacript

---
+++ day 1/objects+arrays.js
@@ -0,0 +1,5 @@
+let cars=[{make: 'Toyota',model: 'Camry', year: 2018}]
+delete cars[0].make;
+cars.push({make: "honda",model:"civic",year:"2020"});
+cars[1].model="Accord";
+console.log(cars);
---
+++ day 1/test.js
@@ -0,0 +1,3 @@
+let x = 'Hello World!!!!';
+console.log(x);
+console.log(typeof x);
